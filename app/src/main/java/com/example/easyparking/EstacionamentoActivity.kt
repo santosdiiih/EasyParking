2c1300bd34fed629eda562f5eb4ede02c4409a7c
ingrid santos <aparecida.rpsilva@gmail.com>
2023-06-15 01:44:12 -0300

    Finalizando os layouts

---
+++ app/src/main/java/com/example/easyparking/EstacionamentoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.easyparking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EstacionamentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_estacionamento)
+    }
+}
